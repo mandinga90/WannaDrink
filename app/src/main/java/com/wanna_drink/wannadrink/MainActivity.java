b2e638aa909cecf8fe0962ef0d065b0cb39c0e59
mandinga90 <manuel@letsgo2.net>
2017-12-02 18:49:15 +0100

    saved in sharedPref

--- app/src/main/java/com/wanna_drink/wannadrink/MainActivity.java
+++ app/src/main/java/com/wanna_drink/wannadrink/MainActivity.java
@@ -1,10 +1,16 @@
 package com.wanna_drink.wannadrink;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import static android.preference.PreferenceManager.getDefaultSharedPreferences;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,8 +23,23 @@ public class MainActivity extends AppCompatActivity {
         btNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, TakePhotoActivity.class));
+                EditText edName = (EditText) findViewById(R.id.ed_name);
+                String name = edName.getText().toString();
+                if(! name.isEmpty()){
+                    saveName(name);
+                    startActivity(new Intent(MainActivity.this, TakePhotoActivity.class));
+                }
+                else{
+                    Toast.makeText(MainActivity.this, R.string.msg_no_name_entered, Toast.LENGTH_SHORT).show();
+                }
             }
         });
+    }
+
+    private void saveName(String name) {
+        SharedPreferences sharedPref = getDefaultSharedPreferences(getApplicationContext());
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString(getString(R.string.key_name),name);
+        editor.commit();
     }
 }
--- app/src/main/java/com/wanna_drink/wannadrink/TakePhotoActivity.java
+++ app/src/main/java/com/wanna_drink/wannadrink/TakePhotoActivity.java
@@ -1,15 +1,30 @@
 package com.wanna_drink.wannadrink;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ImageView;
+import android.util.Base64;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static android.preference.PreferenceManager.getDefaultSharedPreferences;
 
 public class TakePhotoActivity extends AppCompatActivity {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_TAKE_PHOTO = 1;
+    String mCurrentPhotoPath;
+
 //    ImageView mImageView;
 
     @Override
@@ -34,7 +49,31 @@ public class TakePhotoActivity extends AppCompatActivity {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
 //            mImageView.setImageBitmap(imageBitmap);
-            startActivity(new Intent(TakePhotoActivity.this, ChooseDrinkActivity.class));
+//            testSharedPreferences();
+//            startActivity(new Intent(TakePhotoActivity.this, ChooseDrinkActivity.class));
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            imageBitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            byte[] byteArray = stream.toByteArray();
+            saveImageInSharedPref(byteArray);
         }
     }
+
+    private void saveImageInSharedPref(byte[] binaryImg) {
+
+        SharedPreferences sharedPref = getDefaultSharedPreferences(getApplicationContext());
+        SharedPreferences.Editor editor = sharedPref.edit();
+
+        //https://stackoverflow.com/questions/19556433/saving-byte-array-using-sharedpreferences
+        String binaryImgString = Base64.encodeToString(binaryImg, Base64.DEFAULT);
+
+
+        editor.putString(getString(R.string.key_photo),binaryImgString);
+        editor.commit();
+    }
+
+//    private void testSharedPreferences() {
+//        SharedPreferences sharedPref = getDefaultSharedPreferences(getApplicationContext());
+//        String name = sharedPref.getString(getString(R.string.key_name), "");
+//        Toast.makeText(this,"name " + name,Toast.LENGTH_LONG);
+//    }
 }
